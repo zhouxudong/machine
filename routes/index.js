410f7fea0db6fa40dea5888fdab92ab8d028beed
zhouxudong <z_zxdong@163.com>
2016-09-19 22:33:17 +0800

    i18n

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,39 @@ var product_module = require("../module/product")
 
 var getCategoryList = product_module.getCategoryList;
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
   getCategoryList(0).then(function(){
     var response_data = {};
     response_data['title'] = "Best CHINA machine";
-    response_data["categorys"] = arguments[0]
+    response_data["categorys"] = arguments[0];
+    response_data["lg"] = "cn";
+    res.render('index', response_data);
+  })
+
+
+});
+/* GET home page. */
+router.get('/en', function(req, res, next) {
+
+  getCategoryList(0).then(function(){
+    var response_data = {};
+    response_data['title'] = "Best CHINA machine";
+    response_data["categorys"] = arguments[0];
+    response_data["lg"] = "en";
+    res.render('index', response_data);
+  })
+
+
+});
+/* GET home page. */
+router.get('/es', function(req, res, next) {
+
+  getCategoryList(0).then(function(){
+    var response_data = {};
+    response_data['title'] = "Best CHINA machine";
+    response_data["categorys"] = arguments[0];
+    response_data["lg"] = "es";
     res.render('index', response_data);
   })
 
