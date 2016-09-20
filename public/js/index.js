61ce92e61298c131c8cce6a66c0af251e2f76243
zhouxudong <z_zxdong@163.com>
2016-09-20 23:38:18 +0800

    a

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 $("#nav_section_left").on("click","li",function(){
     var id = $(this).data("id");
 
+    var lg = $("#currLg").val()
     $.ajax({
         url: API.getProductsByCategoryId,
         data: {
@@ -8,6 +9,22 @@ $("#nav_section_left").on("click","li",function(){
         },
         success: function(data){
             console.log(data);
+            if(data.response_data){
+                data = data.response_data;
+                var items = data,
+                    allDom = "",item;
+                for(var i=0;i<items.length;i++){
+                    item = items[i];
+                    allDom +=  [
+                        '<div class="item">',
+                        '   <div class="bg_over">',
+                        '       <img width="180px" height="135px" src="'+item.thumb+'">',
+                        '       <p>'+item["name_" + lg] || item.name+'</p>',
+                        '</div></div>'].join("");
+                }
+
+                $(".productList").html(allDom);
+            }
         }
     })
 })
