5e3719a6ee5d39613bbfde034eb6164630baefed
zhouxudong <z_zxdong@163.com>
2016-09-21 11:11:35 +0800

    all

--- routes/index.js
+++ routes/index.js
@@ -10,26 +10,21 @@ var getCategoryList = category_module.getCategoryList;
 var getProductsByCategoryId = product_module.getProductsByCategoryId;
 
 var getIndexResponse = function (res,language){
-  getALLCategorys()
-      .then(function(resolveObj){
 
-        var categorys = resolveObj.data;
-        Promise.all([
-          getCategoryList(0),
-          getProductsByCategoryId(2)
-        ]).then(function(callbacks){
-          var response_data = {};
-          response_data['title'] = "Best CHINA machine";
-          response_data["categorys"] = callbacks[0];
-          response_data["products"] = callbacks[1];
-          response_data["lg"] = language;
-          response_data["api"] = API;
-          res.render("index",response_data);
-        })
-
-      }).catch(function(){
-    res.json({error_code:123})
-  })
+    Promise.all([
+        getCategoryList(0),
+        getProductsByCategoryId(2)
+    ]).then(function(callbacks){
+        var response_data = {};
+        response_data['title'] = "Best CHINA machine";
+        response_data["categorys"] = callbacks[0];
+        response_data["products"] = callbacks[1];
+        response_data["lg"] = language;
+        response_data["api"] = API;
+        res.render("index",response_data);
+    }).catch(function(){
+        res.json({error_code:123})
+    })
 }
 router.get('/', function(req, res, next) {
   getIndexResponse(res,"cn");
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var router = express.Router();
 
+var product_module = require("../module/product");
+var category_module = require("../module/category");
+var getCategoryList = category_module.getCategoryList;
+var getProductById = product_module.getProductById;
+
 router.get("/:id", (req, res, next) => {
 
     var id = req.params.id;
+    Promise.all([
+        getCategoryList(0),
+        getProductById(id)
+    ]).then(function(callbacks){
+        var response_data = {};
+        response_data['title'] = "Best CHINA machine";
+        response_data["categorys"] = callbacks[0];
+        response_data["product"] = callbacks[1];
+        response_data["lg"] = language;
+        response_data["api"] = API;
+        res.render("index",response_data);
+    }).catch(function(){
+        res.json({error_code:123})
+    })
     console.log(id);
     res.render("product");
 })
